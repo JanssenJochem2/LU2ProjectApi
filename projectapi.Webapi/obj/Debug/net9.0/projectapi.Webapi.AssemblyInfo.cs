7053a4cbcb92e0befc550bccd1ffc27109ee5376
JanssenJochem2 <jcm.janssen2@student.avans.nl>
2025-04-07 20:59:15 +0200

    First commit
    First commit in git web api repository

--- projectapi.Webapi/obj/Debug/net9.0/projectapi.Webapi.AssemblyInfo.cs
+++ projectapi.Webapi/obj/Debug/net9.0/projectapi.Webapi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projectapi.Webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b834f168182d654b76f1dab01f95648c951c51a")]
 [assembly: System.Reflection.AssemblyProductAttribute("projectapi.Webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projectapi.Webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
